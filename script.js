93acab3b5e1b33c24ff361d39f9b4863b9cc82d0
Tyler Ralston <tralston511@gmail.com>
2023-12-16 17:02:19 -0500

    script and html additions

---
+++ script.js
@@ -0,0 +1,36 @@
+var today = moment().format("dddd, MMMM Do YYYY ");
+var now = moment().format("H A");
+
+// current date/time
+
+$("#currentDay").text(today);
+
+// planWorkday for each hour of the workday
+
+var planWorkday = [
+    { time: "9 AM", 
+        event: "" },
+    { time: "10 AM", 
+        event: "" },
+    { time: "11 AM", 
+        event: "" },
+    { time: "12 PM", 
+        event: "" },
+    { time: "1 PM", 
+        event: "" },
+    { time: "2 PM", 
+        event: "" },
+    { time: "3 PM", 
+        event: "" },
+    { time: "4 PM", 
+        event: "" },
+    { time: "5 PM", 
+        event: "" },
+  ];
+
+// Local Storage
+
+var workEvents = JSON.parse(localStorage.getItem("workDay"));
+if (workEvents) {
+  planWorkday = workEvents;
+}
