cb306ade55d9ee003fece3e5ac147dd85285a96b
Tyler Ralston <tralston511@gmail.com>
2023-12-18 16:57:32 -0500

    final edits to css, javascript, and html

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Interactive-Daily-Calendar
 An interactive daily calendar designed to schedule your work day based on the 9-5 work schedule. 
+
+## User Story
+Add events to an Interactive Daily Calendar 
+Current or most recent hour is diaplyed in red
+Past hours are displayed in grey
+Future hours are green with sections to edit
+
+## Resources
+- Javasciprt
+- Basic HTML & CSS
+- JQuery 
+
+## Link to Repository 
+https://github.com/TRalston511/Interactive-Daily-Calendar.git
+
+## Link to Application 
+https://tralston511.github.io/Interactive-Daily-Calendar/
+
+## Link to My GitHub
+https://github.com/TRalston511
--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 var today = moment().format("dddd, MMMM Do YYYY ");
 var now = moment().format("H A");
 
-// current date/time
-
-$("#currentDay").text(today);
-
 // planWorkday for each hour of the workday
 
-var planWorkday = [
+var workdayCalendar = [
     { time: "9 AM", 
         event: "" },
     { time: "10 AM", 
@@ -30,7 +26,67 @@ var planWorkday = [
 
 // Local Storage
 
-var workEvents = JSON.parse(localStorage.getItem("workDay"));
-if (workEvents) {
-  planWorkday = workEvents;
+var workdayEvents = JSON.parse(localStorage.getItem("workDay"));
+if (workdayEvents) {
+    workdayCalendar = workdayEvents;
 }
+
+//Current Day
+
+$("#currentDay").text(today);
+
+//Create rows
+
+workdayCalendar.forEach(function(timeBlock, index) {
+	var timeLabel = timeBlock.time;
+	var blockColor = colorRow(timeLabel);
+	var row =
+		'<div class="time-block" id="' +
+		index +
+		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+		timeLabel +
+		'</div><textarea class="form-control ' +
+		blockColor +
+		'">' +
+		timeBlock.event +
+		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+
+	// Adding rows to container div
+
+	$(".container").append(row);
+});
+
+//Color rows based on current time
+
+function colorRow(time) {
+	var calendarNow = moment(now, "H A");
+	var calendarEntry = moment(time, "H A");
+	if (calendarNow.isBefore(calendarEntry) === true) {
+		return "future";
+	} else if (calendarNow.isAfter(calendarEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
+
+// Save Events
+
+$(".saveBtn").on("click", function() {
+	var blockID = parseInt(
+		$(this)
+			.closest(".time-block")
+			.attr("id")
+	);
+	var userEntry = $.trim(
+		$(this)
+			.parent()
+			.siblings("textarea")
+			.val()
+	);
+	workdayCalendar[blockID].event = userEntry;
+
+	// Set local storage
+
+	localStorage.setItem("workDay", JSON.stringify(workdayCalendar));
+});
